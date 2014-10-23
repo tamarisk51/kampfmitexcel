e19cbae4561b35f0a23ae557a2282f4ace18a129
James <jsheppard5189@gmail.com>
2014-10-23 14:24:46 -0500

    updated chi_csvs2xls.py

--- chi_csvs2xls.py
+++ chi_csvs2xls.py
@@ -1,34 +1,60 @@
 import sys, csv, xlwt, os
 cmdargs = str(sys.argv)
 
-
-def write_in_correct_formate(ws,rowx,colx,value):
+def write_row(ws,rowx,row_array):
     """
     Writes the values as either a float or a string, instead of just assuming everything
     is a string.
     """
-    try:
-        float(value)
-        s=False
-    except ValueError:
-        s=True
-    if s:
-        ws.write(rowx, colx, value)
-    else:
-        ws.write(rowx, colx, float(value))
+    for colx, value in enumerate(row_array):
+        try:
+            float(value)
+            type_to_write="Float"
+        except ValueError:
+            type_to_write="String"
+        if type_to_write=="Float":
+            ws.write(rowx, colx, float(value))
+        elif type_to_write=="String":
+            ws.write(rowx, colx, value)
 
 def csvs2xls(directory):
     wb = xlwt.Workbook()
+    
+    # Add 'instructions' tab
+    ws = wb.add_sheet('instructions')
+    write_row(ws,0,["#title","'xlim':(0.2,1.4), 'ylim':(-0.00000000001,0.00000000025)"]) 
+    write_row(ws,1,["file0","background","'linestyle':':', 'linewidth':3.0"])     
+    write_row(ws,2,["file1","1mM analyte"])
+    write_row(ws,3,["file2","2mM analyte"]) 
+    
+    # Write all the csv files in the directory to this workbook.
     for filename in os.listdir(directory):
         if filename.endswith(".csv") or filename.endswith(".txt"):
             ws = wb.add_sheet(os.path.splitext(filename)[0])
             with open('{}\\{}'.format(directory,filename),'rb') as csvfile:
                 reader = csv.reader(csvfile, delimiter=',')
+                skipped_rows=0
                 for rowx, row in enumerate(reader):
-                     for colx, value in enumerate(row):
-                         write_in_correct_formate(ws,rowx,colx,value)
-            
+                    # Skip blank rows
+                    if len(row)<1:
+                        skipped_rows+=1
+                        continue
+                    # Skip text rows we don't want
+                    if ("Segment" in row[0]) or \
+                       ("Header" in row[0]) or \
+                       ("Note" in row[0]) or \
+                       ("Ep" in row[0]) or \
+                       ("ip" in row[0]) or \
+                       ("Ah" in row[0]):
+                        skipped_rows+=1
+                        continue
+                    # Add a blank row before the data
+                    if ("Potential" in row[0]):
+                        skipped_rows-=1
+                    rowx = rowx - skipped_rows
+                    write_row(ws,rowx,row)         
     return wb 
+
 
 if len(sys.argv)==3:
     directory = sys.argv[1]
